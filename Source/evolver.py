719290b33d8d0067513fee4f8e522efee4e6cbc7
Jose <jgh9094@gmail.com>
2024-08-13 10:04:50 -0700

    initial constructor with args

--- Source/evolver.py
+++ Source/evolver.py
@@ -1,14 +1,13 @@
 #####################################################################################################
 #
 # Evolutionary algorithm class that evolves pipelines.
-# We are expecting all numpy related objects
-# Default ints/floats are numpy.int32 and numpy.float32
+# We use the NSGA-II algorithm to evolve pipelines.
+# Pipelines consist of a set of epistatic interactions, feature selector, and a final regressor.
 #
 # Python 3.12.4: conda activate star-epi-pre
 #####################################################################################################
 
 import numpy as np
-import selection
 from typeguard import typechecked
 from typing import List
 import numpy.typing as npt
@@ -26,15 +25,41 @@ class Pipeline:
 
 @typechecked # for debugging purposes
 class EA:
-    def __init__(self, seed: np.int32, pop_size: np.int32, branch_cnt_max: np.int32, selector: selection.Selection):
+    def __init__(self, seed: np.uint16, pop_size: np.uint16, epi_cnt_max: np.uint16,
+                 mut_ran_p: np.float32, mut_smt_p: np.float32, mut_non_p: np.float32,
+                 smt_in_in_p: np.float32, smt_in_out_p: np.float32, smt_out_out_p: np.float32) -> None:
+        """
+        Main class for the evolutionary algorithm.
+
+        Parameters:
+        seed: np.uint16
+            Seed for the random number generator.
+        pop_size: np.uint16
+            Population size.
+        epi_cnt_max: np.uint16
+            Maximum number of epistatic interactions (nodes).
+        mut_ran_p: np.float32
+            Probability for random mutation.
+        mut_smt_p: np.float32
+            Probability for smart mutation.
+        mut_non_p: np.float32
+            Probability for no mutation.
+        smt_in_in_p: np.float32
+            Probability for smart mutation, new snp comes from the same chromosome and assigned bin.
+        smt_in_out_p: np.float32
+            Probability for smart mutation, new snp comes from the same chromosome but different bin.
+        smt_out_out_p: np.float32
+            Probability for smart mutation, new snp comes from a different chromosome (different bin by definition).
+        """
+
         # arguments needed to run
         self.seed = seed
         self.pop_size = pop_size
-        self.rng = np.random.default_rng(seed)
-        self.branch_cnt_max = branch_cnt_max
-
-        # instantiate the population as empty
-        self.population = np.array([])
-
-        # instnatiate the selector passed by user
-        self.selector = selector
+        self.rng = np.random.default_rng(seed) # random number generator to be passed to all other stocastic functions
+        self.epi_cnt_max = epi_cnt_max
+        self.mut_ran_p = mut_ran_p
+        self.mut_smt_p = mut_smt_p
+        self.mut_non_p = mut_non_p
+        self.smt_in_in_p = smt_in_in_p
+        self.smt_in_out_p = smt_in_out_p
+        self.smt_out_out_p = smt_out_out_p
